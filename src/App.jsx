509b43604a7e954f544b6611be0b666f0a7da550
Azizur Rohman <azizurrohman@macbook-air.home>
2024-10-01 15:54:50 +0100

    Add all files and folders

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import { useState } from "react"
+import Background from "./components/Background/Background.jsx"
+import Navbar from "./components/Navbar/Navbar.jsx"
+import Hero from "./components/Hero/Hero.jsx"
+
+const App = () => {
+  let heroData = [
+    {text1:"Dive into",text2:"what you love"},
+    {text1:"Indulge in",text2:"your passions"},
+    {text1:"Give in to",text2:"your dreams"},
+  ]
+
+  const [heroCount, setHeroCount] = useState(2);
+  const [playStatus, setPlayStatus] = useState(false)
+
+  useEffect(()=>{
+    setInterval(() => {
+      setHeroCount((count)=>{return count===2?0:count+1})
+    }, 3000);
+  }, [])
+
+  return (
+    <div>
+      <Background playStatus={playStatus} heroCount={heroCount}/>
+      <Navbar/>
+      <Hero
+        setPlayStatus={setPlayStatus}
+        heroData={heroData[heroCount]}
+        heroCount={heroCount}
+        setHeroCount={setHeroCount}
+        playStatus={playStatus}
+      />
+    </div>
+  )
+}
+
+export default App
